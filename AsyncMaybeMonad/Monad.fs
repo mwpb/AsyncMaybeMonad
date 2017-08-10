a2e0142f57b5d060feb00703f3bf24db6a013514
Matthew Burke <matthewpatrickburke@gmail.com>
2017-08-10 11:33:01 +0100

    Separating out cases.

--- AsyncMaybeMonad/Monad.fs
+++ AsyncMaybeMonad/Monad.fs
@@ -1,27 +1,46 @@
 ﻿module Monad
 
 type MaybeBuilder() =
-    member t.Bind(a:'a option,f:'a->('b option)) = a |> Option.bind(fun x -> f x)
-    member t.Bind(a:System.Threading.Tasks.Task<'a>,f:'a->'b option) = 
-        async {
-                let! b = a |> Async.AwaitTask
-                return f b
-            } |> Async.RunSynchronously
-    member t.Bind(a:System.Threading.Tasks.Task,f:unit->'b option) = 
-        async {
-                do! a |> Async.AwaitTask
-                return f()
-            } |> Async.RunSynchronously
-    member t.Bind(a:Async<'a>,f:'a->'b option) =
+    // If we are binding an option.
+    member t.Bind(a:'a option, f:'a->'b option) = a |> Option.bind(fun x -> f x)
+    member t.Bind(a:'a option, f:'a->Async<'b>) =
+        match a with
+        | Some x ->
             async {
-                let! b = a
-                return f b
-            } |> Async.RunSynchronously
+                let! b = f x
+                return Some b}
+        | None -> async {return None}
+    // If we are binding an Async
+    member t.Bind(a:Async<'a>,f:'a->'b option) =
+        async { let! b = a
+                return f b}
+    member t.Bind(a:Async<'a>,f:'a->Async<'b>) =
+        async{  let! b = a
+                let! c = f b
+                return c}
+    // If we are binding a Task that returns a value
+    member t.Bind(a:System.Threading.Tasks.Task<'a>,f:'a->'b option) = 
+        async { let! b = a |> Async.AwaitTask
+                return f b}
+    member t.Bind(a:System.Threading.Tasks.Task<'a>,f:'a->Async<'b>) = 
+        async { let! b = a |> Async.AwaitTask
+                let! c = f b
+                return c}
+    // If we are binding a Task that doesn't return a value
+    member t.Bind(a:System.Threading.Tasks.Task,f:unit->'b option) = 
+        async { do! a |> Async.AwaitTask
+                return f()}
+    member t.Bind(a:System.Threading.Tasks.Task,f:unit->Async<'b>) = 
+        async { do! a |> Async.AwaitTask
+                return! f()}
+
     member t.Return(a:'a) = if obj.ReferenceEquals(a,null) then None else Some a
     member t.ReturnFrom(x:'a option) = x
+    member t.ReturnFrom(x:Async<'a>) = x
 
 /// Beefed up maybe monad with helper functions in ModelLibrary/Utils.fs.
-/// Returns an option: it is intended that you subsequently pipe to defArg if required.
+/// It returns an option or Async<option> and it is intended that you 
+/// pipe into defArg if required.
 /// It is also intended that you use helper functions to catch exceptions
 /// and convert them to an option type before putting them in the computation expression.
 let maybe = MaybeBuilder()
--- AsyncMaybeMonad/Program.fs
+++ AsyncMaybeMonad/Program.fs
@@ -1,7 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+open System.Diagnostics
+
+open Monad
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let first = maybe {
+        Debug.WriteLine "Started"
+        do! Async.Sleep(3000)
+        Debug.WriteLine "Done"
+        return "First Completed"}
+    let x = Some 2
+    let second = maybe{
+            let! y = x
+            return y
+        }
+    Console.WriteLine first
+    Console.ReadLine() |> ignore
+    0
