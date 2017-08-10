5e9e71a555d5f3cc14e3b76b08da4906883055d0
Matthew Burke <matthewpatrickburke@gmail.com>
2017-08-10 10:54:21 +0100

    Added basic Maybe Monad.

---
+++ AsyncMaybeMonad/Monad.fs
@@ -0,0 +1,27 @@
+﻿module Monad
+
+type MaybeBuilder() =
+    member t.Bind(a:'a option,f:'a->('b option)) = a |> Option.bind(fun x -> f x)
+    member t.Bind(a:System.Threading.Tasks.Task<'a>,f:'a->'b option) = 
+        async {
+                let! b = a |> Async.AwaitTask
+                return f b
+            } |> Async.RunSynchronously
+    member t.Bind(a:System.Threading.Tasks.Task,f:unit->'b option) = 
+        async {
+                do! a |> Async.AwaitTask
+                return f()
+            } |> Async.RunSynchronously
+    member t.Bind(a:Async<'a>,f:'a->'b option) =
+            async {
+                let! b = a
+                return f b
+            } |> Async.RunSynchronously
+    member t.Return(a:'a) = if obj.ReferenceEquals(a,null) then None else Some a
+    member t.ReturnFrom(x:'a option) = x
+
+/// Beefed up maybe monad with helper functions in ModelLibrary/Utils.fs.
+/// Returns an option: it is intended that you subsequently pipe to defArg if required.
+/// It is also intended that you use helper functions to catch exceptions
+/// and convert them to an option type before putting them in the computation expression.
+let maybe = MaybeBuilder()
